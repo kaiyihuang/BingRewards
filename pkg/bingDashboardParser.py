5a8fb6b756f7df915dbd8fbd7103b3b8ac66d486
Kenney He <kenneyhe@gmail.com>
2017-11-03 10:41:21 -0700

    spacing and adding encoding ignore of latin-1

--- pkg/bingDashboardParser.py
+++ pkg/bingDashboardParser.py
@@ -139,7 +139,7 @@ def parseDashboardPage(page, bing_url):
         descriptionDiv = ddiv.find('div', class_='spacer-12-top')
         if descriptionDiv is not None:
             rewardDescription = descriptionDiv.get_text()
-        createReward(currentReward, rewardURL, rewardName, rewardProgressCurrent, rewardProgressMax, rewardDescription)
+        (currentReward, rewardURL, rewardName, rewardProgressCurrent, rewardProgressMax, rewardDescription)
         allRewards.append(currentReward)
 
     # Get the rewards on the main dashboard page
@@ -171,8 +171,8 @@ def parseDashboardPage(page, bing_url):
                                                   class_='offer-description-height spacer-20-top offer-description-margin-bottom')
                 # if the reward is a quiz and fully complete
         if rewardDescription.get_text().find('You did it!') != -1:
-            rDscSplit = rewardDescription.get_text().split(' ')
-            rewardProgressCurrent = rewardProgressMax = int(rDscSplit[rDscSplit.index('points.') - 1])
+            rDscSplit=rewardDescription.get_text().replace('\n', '').split(' ')
+            rewardProgressCurrent = rewardProgressMax = int(rDscSplit[rDscSplit.index('points.') - 1].replace(',',''))
             # Grab the point totals for 'HIT' rewards - we're using these as a marker to set the 'HIT' type
         hits = checkForHit(currAction, rewardProgressCurrent, rewardProgressMax, links[i])
         if hits is not None:
@@ -236,10 +236,10 @@ def checkForHit(currAction, rewardProgressCurrent, rewardProgressMax, searchLink
 
 def createReward(reward, rUrl, rName, rPC, rPM, rDesc):
     reward.url = rUrl.strip()
-    reward.name = rName.strip()
+    reward.name = rName.strip().encode('latin-1', 'ignore')
     reward.progressCurrent = rPC
     reward.progressMax = rPM
-    reward.description = rDesc.strip()
+    reward.description = rDesc.strip().encode('latin-1', 'ignore')
     if rPC == rPM:
         reward.isDone = True
 
